fc2a7278c558f7866ae309fcb0d7e4fa6f582825
Shafi-Hub <shafihaleelguraishi@gmail.com>
2022-02-21 11:40:30 +0530

    added template folder and static folder

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect, request
 from markupsafe import escape
 
 vac = Flask(__name__)
+
+@vac.route('/')
+def home_Page():
+    return "Hello Python"

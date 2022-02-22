f5b6e728881f81a39f844f7259c3ea1d1ff3ee99
Shafi-Hub <shafihaleelguraishi@gmail.com>
2022-02-23 00:05:13 +0530

    signup page, personal information design almost completed

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from markupsafe import escape
 
 vac = Flask(__name__)
 
+
 @vac.route('/')
-def home_Page():
+def home_page_():
     return render_template('index.html')
+
+@vac.route('/sign_up/', methods=['GET', 'POST'])
+def signup_():
+    if request.method == 'POST':
+        signup_form_submit()
+    else:
+        return render_template('signup.html')
+
+

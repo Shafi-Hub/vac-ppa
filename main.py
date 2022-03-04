328fb79ec5c2477f4d026a709618a09847b06257
Shafi-Hub <shafihaleelguraishi@gmail.com>
2022-03-04 12:39:26 +0530

    Address page completed without database connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
-from Modules.SignUp.signup import signup_form_Personal_info
+from Modules.SignUp.signup import signup_form_submit, signup_form_Personal_info, signup_form_Address_info
 
 vac = Flask(__name__)
 vac.secret_key = b'shdihdwq$%%^&^%fhkhjhk&(*)&*(&^&$%%#$%$#^VKJGVJGFVGJVFJHBK'
@@ -9,12 +9,21 @@ vac.secret_key = b'shdihdwq$%%^&^%fhkhjhk&(*)&*(&^&$%%#$%$#^VKJGVJGFVGJVFJHBK'
 def home_page_():
     return render_template('index.html')
 
-@vac.route('/sign_up/', methods=['GET', 'POST'])
-def signup_():
+
+@vac.route('/signup_personal/', methods=['GET', 'POST'])
+def _signup_form_personal_info_():
     if request.method == 'POST':
         signup_form_Personal_info()
-        return render_template('signup_addr_info.html')
+        return render_template('signup/signup_addr_info.html')
     else:
-        return render_template('signup_personal_info.html')
+        return render_template('signup/signup_personal_info.html')
 
+@vac.route('/signup_address/', methods=['GET', 'POST'])
+def _signup_form_Address_info_():
+
+    if request.method == 'POST':
+        signup_form_Address_info()
+        return render_template('signup/signup_qualification_info.html')
+    else:
+        return render_template('signup/signup_addr_info.html')
 
